953272ae0db5dd63040d18f7a9801496c405fc8e
soorajkumarluhana <soorajkumarluhana@gmail.com>
2021-06-25 18:52:37 -0700

    done With Sign up Page

--- foodorderingapp/lib/main.dart
+++ foodorderingapp/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:foodorderingapp/LoginPage.dart';
+import 'package:foodorderingapp/SignupPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SignUp(),
     );
   }
 }
